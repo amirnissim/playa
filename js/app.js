414d005a348749a0c5d711d0942996e3de9e576f
Amir Nissim <amirnissim@gmail.com>
2014-09-10 17:14:58 +0300

    use Github gists to store data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular.module('Playa', [])
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, JSONStore) {
 
     $scope.appName = 'EverythingMe Launcher';
 
@@ -7,6 +7,51 @@ angular.module('Playa', [])
                             'you need it, right to your homescreen.';
     $scope.developerName = 'EverythingMe';
 
-    $scope.installText = 'Install';
+    // history for saved configurations. an array of object like:
+    // {
+    //  id: str
+    //  href: url
+    // }
+    $scope.history = [];
+
+    $scope.save = function save() {
+      return JSONStore
+      .add({
+        appName: $scope.appName,
+        appDescription: $scope.appDescription,
+        appDescription: $scope.appDescription,
+        developerName: $scope.developerName
+      })
+      .success(function(result) {
+        $scope.history.push({
+          id: result.id,
+          href: result.html_url
+        });
+      });
+    };
+  })
+
+  /**
+   * JSON storage backend using Github Gists API
+   */
+  .service('JSONStore', function($http) {
+    var URL = 'https://api.github.com/gists';
+
+    this.get = function save(key) {
+      return $http.get(URL + '/' + key);
+    };
+
+    this.add = function add(obj) {
+      var json = JSON.stringify(obj);
+
+      return $http.post(URL, {
+        "public": true,
+        "files": {
+          "data.json": {
+            "content": json
+          }
+        }
+      });
+    };
 
   });
